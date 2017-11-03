6d41b9d32e0baf35a782dda8e43468cc531aa0ad
Markus Brand <markusbrand97@gmail.com>
2017-11-03 13:40:10 +0100

    add authors controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'authors/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  resources :authors
+
   root 'home#index'
  
   # Example of regular route:
