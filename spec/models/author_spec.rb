237e966184f51ddd536df621133e022f33ca10e7
Markus Brand <markusbrand97@gmail.com>
2017-11-03 17:22:11 +0100

    Refs issue #11: Add a test for invalid authors

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,4 +9,9 @@ describe 'Author Object', type: :model do
     expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
     expect(author.name).to eq('Alan Turing')
   end
+
+  it 'should not be able to create an author without last name' do
+    author = Author.new(first_name: 'Alan', last_name: '', homepage:'test')
+    expect(author).not_to be_valid
+  end
 end
