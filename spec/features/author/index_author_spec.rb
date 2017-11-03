079b1af2e2053fc7066055b57f42ee8442f0d9fe
Markus Brand <markusbrand97@gmail.com>
2017-11-03 15:56:44 +0100

    Refs issue #8: Add a test for searching Alan in list

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -6,4 +6,11 @@ describe "Author detail page", type: :feature do
     visit authors_path
   end
 
+  it "should show_data" do
+    FactoryGirl.create :author
+    visit authors_path
+
+    expect(page.text).to match(/alan turing/i)
+  end
+
 end
