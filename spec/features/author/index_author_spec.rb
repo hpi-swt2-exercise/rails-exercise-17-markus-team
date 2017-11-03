43a601adcc6c3976cd5738e76e6817fb148163be
Markus Brand <markusbrand97@gmail.com>
2017-11-03 19:41:23 +0100

    Refs issue #15: Add a test for deleting authors

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
-describe "Author index page", type: :feature do
-
-  it "should render withour error" do
+describe 'Author index page', type: :feature do
+  it 'should render withour error' do
     visit authors_path
   end
 
-  it "should show_data" do
+  it 'should show_data' do
     FactoryGirl.create :author
     visit authors_path
 
     expect(page.text).to match(/alan turing/i)
   end
 
-  it "should have a link to new author" do
+  it 'should have a link to new author' do
     FactoryGirl.create :author
     visit authors_path
 
@@ -21,25 +20,33 @@ describe "Author index page", type: :feature do
     expect(page).to have_link('Show')
   end
 
-  it "should have a link to show author" do
+  it 'should have a link to show author' do
     FactoryGirl.create :author
     visit authors_path
 
     expect(page).to have_link('Show')
   end
 
-  it "should have a link to edit author" do
+  it 'should have a link to edit author' do
     FactoryGirl.create :author
     visit authors_path
 
     expect(page).to have_link('Edit')
   end
 
-  it "should have a link to destroy author" do
+  it 'should have a link to destroy author' do
     FactoryGirl.create :author
     visit authors_path
 
     expect(page).to have_link('Destroy')
   end
 
+  it 'should actually remove the author' do
+    @alan = FactoryGirl.create :author
+    visit authors_path
+
+    click_link('Destroy')
+
+    expect(@alan).to be_nil
+  end
 end
