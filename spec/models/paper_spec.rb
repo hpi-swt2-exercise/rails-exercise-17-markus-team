5362230066347958ac5adb855b5499dd3e70687c
Markus Brand <markusbrand97@gmail.com>
2017-11-03 21:24:40 +0100

    Refs #17: Change the test to use year as integer

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe 'Paper Object', type: :model do
 
   it 'should create test paper' do
-    paper = Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: 'Mind 49: 433-460', year:'1950')
+    paper = Paper.new(title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: 'Mind 49: 433-460', year:1950)
     expect(paper.title).to eq('COMPUTING MACHINERY AND INTELLIGENCE')
     expect(paper.venue).to eq('Mind 49: 433-460')
-    expect(paper.year).to eq('1950')
+    expect(paper.year).to eq(1950)
   end
 
 end
