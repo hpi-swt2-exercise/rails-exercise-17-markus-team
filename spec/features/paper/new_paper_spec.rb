5cb7f0f69c5f32101c8525be2723dff592d99350
Markus Brand <markusbrand97@gmail.com>
2017-11-03 22:03:15 +0100

    Refs #20: Add save paper input test

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -12,4 +12,17 @@ describe 'New Paper page', type: :feature do
     expect(page).to have_field('Venue')
     expect(page).to have_field('Year')
   end
+
+  it 'should save input' do
+    visit new_paper_path
+    fill_in 'paper_title', with: 'COMPUTING MACHINERY AND INTELLIGENCE'
+    fill_in 'paper_venue', with: 'Mind 49: 433-460'
+    fill_in 'paper_year', with: '1950'
+
+    expect(Paper.find_by_first_name('COMPUTING MACHINERY AND INTELLIGENCE')).to be_nil
+
+    find('input[type="submit"]').click
+
+    expect(Paper.find_by_first_name('COMPUTING MACHINERY AND INTELLIGENCE')).not_to be_nil
+  end
 end
