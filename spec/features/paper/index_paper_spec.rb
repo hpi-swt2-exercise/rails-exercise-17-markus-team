ee26420783162220c04beba2802bdb60de65c0d8
Markus Brand <markusbrand97@gmail.com>
2017-11-03 22:32:09 +0100

    Refs #22: Add paper link test

--- spec/features/paper/index_paper_spec.rb
+++ spec/features/paper/index_paper_spec.rb
@@ -19,35 +19,35 @@ describe 'Paper index page', type: :feature do
     expect(page).to have_link('Add paper')
   end
 
-  it 'should have a link to show author' do
+  it 'should have a link to show paper' do
     FactoryGirl.create :paper
     visit papers_path
 
     expect(page).to have_link('Show')
   end
+
+  it 'should have a link to edit paper' do
+    FactoryGirl.create :paper
+    visit papers_path
+
+    expect(page).to have_link('Edit')
+  end
 #
-#  it 'should have a link to edit author' do
-#    FactoryGirl.create :author
-#    visit authors_path
-#
-#    expect(page).to have_link('Edit')
-#  end
-#
-#  it 'should have a link to destroy author' do
-#    FactoryGirl.create :author
-#    visit authors_path
+#  it 'should have a link to destroy paper' do
+#    FactoryGirl.create :paper
+#    visit papers_path
 #
 #    expect(page).to have_link('Destroy')
 #  end
 #
-#  it 'should actually remove the author' do
-#    FactoryGirl.create :author
-#    visit authors_path
+#  it 'should actually remove the paper' do
+#    FactoryGirl.create :paper
+#    visit papers_path
 #
-#    expect(Author.find_by_first_name('Alan')).not_to be_nil
+#    expect(paper.find_by_first_name('Alan')).not_to be_nil
 #
 #    click_link('Destroy')
 #
-#    expect(Author.find_by_first_name('Alan')).to be_nil
+#    expect(paper.find_by_first_name('Alan')).to be_nil
 #  end
 end
