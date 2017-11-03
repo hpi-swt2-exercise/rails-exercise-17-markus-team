55dd46ec3993f64bde55c6b31fc7e4a7d7a63631
Markus Brand <markusbrand97@gmail.com>
2017-11-03 20:53:55 +0100

    Refs #16: Add a test

---
+++ spec/features/paper/new_paper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'New Paper page', type: :feature do
+  it 'should render without error' do
+    visit new_paper_path
+  end
+
+  it 'should have text input for title, venue and year' do
+    visit new_paper_path
+
+    expect(page).to have_field('Title')
+    expect(page).to have_field('Venue')
+    expect(page).to have_field('Year')
+  end
+end
