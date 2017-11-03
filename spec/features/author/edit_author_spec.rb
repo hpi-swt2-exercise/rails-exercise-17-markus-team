1e664ec5a6cb55c9452903300182633eefd3101b
Markus Brand <markusbrand97@gmail.com>
2017-11-03 18:32:10 +0100

    Refs issue #14: Red test

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 describe "Edit author page", type: :feature do
-  
-  it "should render withour error" do
+
+  it "should render without error" do
     FactoryGirl.create :author
     visit edit_author_path(id: "1")
+  end
+
+  it "should edit without error" do
+    @alan = FactoryGirl.create :author
+    visit edit_author_path(@alan)
+    fill_in "author_first_name", :with => 'Alan Mathison'
+
+    find('input[type="submit"]').click
   end
 
 end
